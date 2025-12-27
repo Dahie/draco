d61335916a4707dea31b6ffcac1e1880727455d5
Daniel Senff <mail@danielsenff.de>
2025-12-27 08:50:28 +0100

    Fixes audio in samples to work with dagonruby v241
    Running the sample in the current dragonruby version 241, it crashes with the message the used `queue_sound` method on the GTK runtime is missing a parameter.
    Sometime in the last year, the `queue_sound` method changed expecting a gain (float 0-1) parameter.
    See https://github.com/DragonRuby/dragonruby-game-toolkit-contrib/blob/main/dragon/runtime.rb#L886
    
    This did not impact the public interface using `arg.outputs.sounds << "path"` notation, which used in all documentation and examples.
    I therefore switched the code to this notation and now the example is working fine again.

--- samples/teeny-tiny/app/systems/handle_destroyed.rb
+++ samples/teeny-tiny/app/systems/handle_destroyed.rb
@@ -6,7 +6,7 @@ class HandleDestroyed < Draco::System
       explosion = Explosion.new(position: {x: entity.position.x, y: entity.position.y })
       world.entities.delete(entity)
       world.entities.add(explosion)
-      args.gtk.queue_sound("sounds/explosion.ogg")
+      args.outputs.sounds << "sounds/explosion.ogg"
     end
   end
 end
